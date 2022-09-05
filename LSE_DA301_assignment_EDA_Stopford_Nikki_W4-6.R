d10174e85312d8bf8c4d7ce8968ace7338ab3037
Nikki Stopford <103060811+BooBN1@users.noreply.github.com>
2022-09-05 16:33:55 +0100

    Add files via upload

--- LSE_DA301_assignment_EDA_Stopford_Nikki_W4-6.R
+++ LSE_DA301_assignment_EDA_Stopford_Nikki_W4-6.R
@@ -1,3 +1,4 @@
+# Week 4
 #Business question 4: What impact does each product have on sales?
 
 #Install and import the Tidyverse library
@@ -11,20 +12,28 @@ turtle_sales <- read.csv("turtle_sales.csv", header=TRUE)
 turtle_sales
 
 # View the data frame.
+head(turtle_sales)
 View(turtle_sales) 
 
 # Explore the data
+dim(turtle_sales)
 glimpse(turtle_sales)
 summary(turtle_sales)
 
-# Subset the data by dropping redundant columns
+# Subset the data by dropping redundant columns and save as a new data frame
 sales <- select(turtle_sales, -Ranking, -Year, -Genre, -Publisher)
+
+# Explore the data for missing values (output shows no missing values)
+sum(is.na(sales))
 
 # View and explore the subset dataframe
 View(sales)
 head(sales)
 summary(sales)
 as_tibble(sales)
+
+# Generate a downloadable HTML file containing summary stats of the data subset
+DataExplorer::create_report(sales)
 
 # Create plots to review and determine insights into the sales data set.
 # Create a scatterplots
@@ -47,19 +56,23 @@ qplot(Global_Sales, data=sales, colour=I('red'), geom='boxplot')
 
 # Determine the impact on sales per product_id
 # Use the group_by and aggregate functions to sum the values grouped by product.
-sales_sum <- sales %>% group_by(Product) %>% summarise(across(.cols = c(NA_Sales, EU_Sales, Global_Sales), list(sum = sum)))
+sales_sum <- sales %>% group_by(Product) %>%
+  summarise(NA_Sales_sum=sum(NA_Sales),
+            EU_Sales_sum=sum(EU_Sales),
+            Global_Sales_sum=sum(Global_Sales),
+            .groups='drop')
+
+# View the results.
+sales_sum
 
 # Create a summary of the new data frame.
-head(as_tibble(sales_sum))
-View(sales_sum)
-glimpse(sales_sum)
 summary(sales_sum)
 
 # Create plots to review and determine insights into the sales data set.
 # Create a scatterplots
-qplot(NA_Sales_sum, EU_Sales_sum, data=sales_sum)
-qplot(NA_Sales_sum, Global_Sales_sum, data=sales_sum)
-qplot(Global_Sales_sum, EU_Sales_sum, data=sales_sum, colour=I('red'))
+qplot(NA_Sales_sum, EU_Sales_sum, data=sales_sum, colour=I('red'))
+qplot(NA_Sales_sum, Global_Sales_sum, data=sales_sum, colour=I('purple'))
+qplot(Global_Sales_sum, EU_Sales_sum, data=sales_sum, colour=I('blue'))
 
 # Create histograms
 qplot(NA_Sales_sum, bins=10, data=sales_sum)
@@ -74,8 +87,137 @@ qplot(Global_Sales_sum, data=sales_sum, colour=I('red'), geom='boxplot')
 # Note your observations and diagrams that could be used to provide insights into the business.
 ## Observations and charts noted in assignment report for Business question 4.
 
+###################################################################################
 
+# Week 5
+# Business question 4: What impact does each product have on sales?  
+# Business question 5: How reliable is the data (e.g. normal distribution, skewness or kurtosis)?
 
+# Load and explore the subset sales data from week 4. 
+# View the data frame to sense-check the data set.
+View(sales_sum)
+head(sales_sum)
+summary(sales_sum)
+as_tibble(sales_sum)
+  
+# Determine the min, max and mean values of all the sales data (three columns).
+# North American sales mean, median min and max
+mean(sales_sum$NA_Sales_sum)
+median(sales_sum$NA_Sales_sum)
+min(sales_sum$NA_Sales_sum)
+max(sales_sum$NA_Sales_sum)
 
+# EU sales mean, median min and max
+mean(sales_sum$EU_Sales_sum)
+median(sales_sum$EU_Sales_sum)
+min(sales_sum$EU_Sales_sum)
+max(sales_sum$EU_Sales_sum)
 
+# Global sales mean, median min and max
+mean(sales_sum$Global_Sales_sum)
+median(sales_sum$Global_Sales_sum)
+min(sales_sum$Global_Sales_sum)
+max(sales_sum$Global_Sales_sum)
 
+# Create a summary of the data frame.
+summary(sales_sum)
+
+# As also done in Wk4: Determine the impact on sales per product_id
+# Use the group_by, apply(), and/or aggregate functions to sum the values grouped by product. 
+sales_sum <- sales %>% group_by(Product) %>%
+  summarise(NA_Sales_sum=sum(NA_Sales),
+            EU_Sales_sum=sum(EU_Sales),
+            Global_Sales_sum=sum(Global_Sales),
+            .groups='drop')
+
+# View the results.
+sales_sum
+
+# Create a summary of the data frame.
+summary(sales_sum)
+
+# Create plots explore normality in the data set.
+# Create a historgram plot.
+hist(sales_sum$Global_Sales_sum) +
+  geom_smooth(se=FALSE)
+  
+# Create a boxplot.
+boxplot(sales_sum$Global_Sales_sum) +
+  title("Boxplot of global product sales")
+  theme_classic()
+  
+# Create a quantile-quantile plot with qqline
+qqnorm(sales_sum$Global_Sales_sum) 
+qqline(sales_sum$Global_Sales_sum) 
+
+# Determine the normality of the data set (sales data).
+# Perform a Shapiro-Wilk test on all the sales data.
+shapiro.test(sales_sum$NA_Sales_sum)
+shapiro.test(sales_sum$EU_Sales_sum)
+shapiro.test(sales_sum$Global_Sales_sum)
+
+# Determine the Skewness and Kurtosis of all the sales data.
+# Install the moments package and load the library.
+install.packages('moments') 
+library(moments)
+
+# Specify the skewness and kurtosis functions.
+# North American sales skewness and kurtosis
+skewness(sales_sum$NA_Sales_sum) 
+kurtosis(sales_sum$NA_Sales_sum)
+
+# EU Sales skewness and kurtosis
+skewness(sales_sum$EU_Sales_sum) 
+kurtosis(sales_sum$EU_Sales_sum)
+
+# Global sales skewness and kurtosis
+skewness(sales_sum$Global_Sales_sum) 
+kurtosis(sales_sum$Global_Sales_sum)
+
+# Determine if there is any correlation between the sales data columns.
+cor(sales_sum$Global_Sales_sum, sales_sum$NA_Sales_sum)
+cor(sales_sum$Global_Sales_sum, sales_sum$EU_Sales_sum)
+cor(sales_sum$EU_Sales_sum, sales_sum$NA_Sales_sum)
+
+# Our correlation coefficients of 0.92, 0.85 and 0.62 
+# This suggests a strong positive correlation.
+
+# Create plots to gain insights into the sales data.
+# Install and import the ggplot2 package.
+install.packages('ggplot2')
+library(ggplot2)
+
+# Create a scatterplot to demonstrate relationship between NA sales and EU sales
+# Exclude standard error
+ggplot(sales, aes(x=NA_Sales, y=EU_Sales,)) + 
+  geom_point() +
+  geom_smooth(se=FALSE) +
+  ggtitle("Relationship between product sales in North America and Europe") +
+  theme_classic()
+  
+################################################################################
+
+# Week 6
+
+# Business question 6: What are the relationships, if any, between North American, European and global sales?
+
+# Load and explore the week 5 data frame 
+
+# Create a simple linear regression model.
+  # Determine the correlation between the sales columns.
+  # View the output.
+  # Create plots to view the linear regression.
+
+# Create a multiple linear regression model.
+  # Select only the numeric columns.
+  # Determine the correlation between the sales columns.
+  # View the output.
+
+# Predict global sales based on provided values. Compare your prediction to the observed value(s).
+  # NA_Sales_sum of 34.02 and EU_Sales_sum of 23.80.
+  # NA_Sales_sum of 3.93 and EU_Sales_sum of 1.56.
+  # NA_Sales_sum of 2.73 and EU_Sales_sum of 0.65.
+  # NA_Sales_sum of 2.26 and EU_Sales_sum of 0.97.
+  # NA_Sales_sum of 22.08 and EU_Sales_sum of 0.52.
+
+# Summarise (150–200 words) any insights you’ve discovered as well as anything you would like to explore further.
